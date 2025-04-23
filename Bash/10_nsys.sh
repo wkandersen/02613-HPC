57666a7f6a3ecb4fbe591a066b7a8d64a0840b79
Nana Villinger <s224227@dtu.dk>
2025-04-23 20:47:47 +0200

    nsys

--- Bash/10_nsys.sh
+++ Bash/10_nsys.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#BSUB -q c02613
+#BSUB -q gpua10 
 #BSUB -J gpujob
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 00:05
+#BSUB -W 00:30
 #BSUB -o 10_nsys_%J.out
 #BSUB -e 10_nsys_%J.err
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 
-# Run the profiler
-nsys profile -o 10_nsys python 02613-HPC/py_filer/9_cupy.py 10
+nsys profile -o 10_nsys python py_filer/9_cupy.py 10
+nsys stats 10_nsys.nsys-rep
