f9cc6c0a972e51189599228a26f798299d53b0a0
Nana Villinger <s224227@dtu.dk>
2025-04-30 11:07:46 +0200

    clean up and 10_fix

--- Bash/10_nsys_fix.sh
+++ Bash/10_nsys_fix.sh
@@ -6,10 +6,10 @@
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 00:30
-#BSUB -o 10_nsys_%J.out
-#BSUB -e 10_nsys_%J.err
+#BSUB -o 10_nsys_fix_%J.out
+#BSUB -e 10_nsys_fix_%J.err
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 
-nsys profile -o 10_nsys python py_filer/9_cupy.py 10
-nsys stats 10_nsys.nsys-rep
+nsys profile -o 10_nsys_fix python py_filer/10_cupy_fix.py 10
+nsys stats 10_nsys_fix.nsys-rep
---
+++ Bash/6_parallelize_time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#BSUB -J parallelize_time
+#BSUB -q hpc
+#BSUB -W 5
+#BSUB -R "rusage[mem=2G]" 
+#BSUB -R "select[model == XeonGold6126]"
+#BSUB -R "span[hosts=1]"
+#BSUB -n 1
+#BSUB -o parallelize_time_%J.out
+#BSUB -e parallelize_time_%J.err
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)/py_filer
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python py_filer/6_calculations.py "6_parallelize_24833978.err"
