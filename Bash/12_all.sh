fbb1f00f4691063623e903d818a3cc1f97283232
Nana Villinger <s224227@dtu.dk>
2025-04-30 11:22:09 +0200

    clean up

--- Bash/12_all.sh
+++ Bash/12_all.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#BSUB -q gpua10
+#BSUB -q c02613
 #BSUB -J gpujob
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=4GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 6:00
+#BSUB -W 3:00
 #BSUB -o 12_cupy_%J.out
 #BSUB -e 12_cupy_%J.err
 source /dtu/projects/02613_2025/conda/conda_init.sh
